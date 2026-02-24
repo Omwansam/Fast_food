f2b676d8667f7ddc15738bf24bc7874984cf066a
Omwansam <omwansamarnold@gmail.com>
2026-02-24 19:20:11 +0300

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,12 @@
-import { Tabs } from 'expo-router';
+import { Redirect, Slot } from 'expo-router';
 import React from 'react';
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+export default function _layout() { 
+    const isAuthenticated = false; // Replace with your authentication logic
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    if (!isAuthenticated) 
+        return <Redirect href="/sign-in" />
+        // If the user is not authenticated, you can redirect them to the sign-in page
+        // or show a message indicating that they need to sign in.
+  return <Slot />
 }
