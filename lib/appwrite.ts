d19d1159772353af4ed3b19b4834e1fdd25e7b68
Omwansam <omwansamarnold@gmail.com>
2026-02-28 13:03:40 +0300

    Sign up and sign in pages

---
+++ lib/appwrite.ts
@@ -0,0 +1,10 @@
+
+
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  platform: "com.jpm.foodordering",
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  projectName: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_NAME,
+  databaseId: '699e148f0015375a1718',
+  userCollectionId: 'user'
+}
